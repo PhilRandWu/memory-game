c0b4dd2974c3ddcf7226e1e499b3e6756d8e34ef
Wenbo.Wu <3378814052@qq.com>
2023-12-18 11:51:28 +0800

    fix: game rules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,55 @@
+use slint::{Timer, VecModel};
+use std::rc::Rc;
+use std::time::Duration;
 slint::include_modules!();
 
 fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+    use slint::Model;
+    let main_window = MainWindow::new().unwrap();
+
+    // Fetch the tiles from the model
+    let mut tiles: Vec<TileData> = main_window.get_memory_tiles().iter().collect();
+    // Duplicate them to ensure that we have pairs
+    tiles.extend(tiles.clone());
+
+    // Randomly mix the tiles乱 tiles
+    use rand::seq::SliceRandom;
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+
+    let tiles_model = Rc::new(VecModel::from(tiles));
+    main_window.set_memory_tiles(tiles_model.clone().into());
+
+    let main_window_weak = main_window.as_weak();
+
+    main_window.on_check_if_pair_solved(move || {
+        let mut flipped_tiles = tiles_model
+            .iter()
+            .enumerate()
+            .filter(|(_, tile)| tile.image_visible && !tile.solved);
+        if let (Some((t1_idx, mut t1)), Some((t2_idx, mut t2))) =
+            (flipped_tiles.next(), flipped_tiles.next())
+        {
+            let is_pair_solved = t1 == t2;
+            if is_pair_solved {
+                t1.solved = true;
+                tiles_model.set_row_data(t1_idx, t1);
+                t2.solved = true;
+                tiles_model.set_row_data(t2_idx, t2);
+            } else {
+                main_window_weak.unwrap().set_disable_tiles(true);
+                let main_window_weak = main_window_weak.clone();
+                let tiles_model_clone = tiles_model.clone();
+                Timer::single_shot(Duration::from_secs(1), move || {
+                    main_window_weak.unwrap().set_disable_tiles(false);
+                    t1.image_visible = false;
+                    tiles_model_clone.set_row_data(t1_idx, t1);
+                    t2.image_visible = false;
+                    tiles_model_clone.set_row_data(t2_idx, t2);
+                });
+            }
+        }
+    });
+
+    main_window.run().unwrap();
 }
